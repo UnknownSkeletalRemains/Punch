d7ce3a1af7b78fd0017a13a62ee5b5fec5da6493
Kirya <29959059+UnknownSkeletalRemains@users.noreply.github.com>
2023-02-18 19:27:03 +0300

    Add files via upload

--- src/app/boards/contentScene.php
+++ src/app/boards/contentScene.php
@@ -3,34 +3,15 @@ namespace app\boards;
 
 use php\gui\{UXForm, shape\UXRectangle, layout\UXFlowPane};
 
-class mainViewBoard
+class contentScene
 {
-    public $mainView, $mainViewSize = [256, 512], $boardRect, $fragment;
+    public $view, $viewSize = [256, 512], $boardRect, $fragment;
     public function __construct() {
-        //Инициализация окна
-        $this->mainView = new UXForm();
-        $this->mainView->title = "Punch";
-        $this->mainView->size = $this->mainViewSize;
-        $this->mainView->layout->backgroundColor = "TRANSPARENT";
-        $this->mainView->transparent = true;
-        $this->mainView->style = "TRANSPARENT";
-        $this->mainView->show();
-
-        $flowPane = new UXFlowPane();
-        $flowPane->size = [256, 32];
-
-        $flowPane->position = [0, 0];
-
-        // Добавляем объект на форму
-        $this->add($flowPane);
-        
-        $this->boardRect = new UXRectangle();
-        $this->boardRect->size = $this->mainView->size;
-        $this->boardRect->arcHeight = 64;
-        $this->boardRect->arcWidth = 64;
-        $this->boardRect->strokeType = 'INSIDE';
-        $this->boardRect->strokeWidth = 2;
-        //$this->boardRect->style = '-fx-fill: linear-gradient(to top, #0d0d0d, black); -fx-stroke: #ffffff0e';
-        $this->mainView->add($this->boardRect);
+        $this->view = new UXForm();
+        $this->view->title = "Punch";
+        $this->view->size = $this->viewSize;
+        $this->view->layout->backgroundColor = "TRANSPARENT";
+        $this->view->transparent = true;
+        $this->view->style = "TRANSPARENT";
     }
 }
--- src/app/boards/mainViewBoard.php
+++ src/app/boards/mainViewBoard.php
@@ -1,37 +1,59 @@
 <?php
 namespace app\boards;
 
-use php\gui\{UXForm, shape\UXRectangle, layout\UXFragmentPane};
 
+use php\gui\{UXForm, shape\UXRectangle, layout\UXFragmentPane, layout\UXFlowPane};
+use behaviour\custom\DraggingFormBehaviour;
+use php\gui\paint\UXPaint;
 use app\boards\contentScene;
 
 class mainViewBoard
 {
-    public $mainView, $mainViewSize = [256, 512], $boardRect, $fragment;
+    public $view, $viewSize = [256, 512], $boardRect, $fragment;
     public function __construct() {
         //Инициализация окна
-        $this->mainView = new UXForm();
-        $this->mainView->title = "Punch";
-        $this->mainView->size = $this->mainViewSize;
-        $this->mainView->layout->backgroundColor = "TRANSPARENT";
-        $this->mainView->transparent = true;
-        $this->mainView->style = "TRANSPARENT";
-        $this->mainView->show();
+        $this->view = new UXForm();
+        $this->view->title = "Punch";
+        $this->view->size = $this->viewSize;
+        $this->view->layout->backgroundColor = "TRANSPARENT";
+        $this->view->transparent = true;
+        $this->view->style = "TRANSPARENT";
+        $this->view->show();
 
         $this->boardRect = new UXRectangle();
-        $this->boardRect->size = $this->mainView->size;
+        $this->boardRect->size = $this->viewSize;
         $this->boardRect->arcHeight = 64;
         $this->boardRect->arcWidth = 64;
-        $this->boardRect->strokeType = 'INSIDE';
+        $this->boardRect->strokeType = "INSIDE";
         $this->boardRect->strokeWidth = 2;
-        $this->boardRect->style = '-fx-fill: linear-gradient(to top, #0d0d0d, black); -fx-stroke: #ffffff0e';
-        $this->mainView->add($this->boardRect);
+        $this->boardRect->style = "-fx-fill: linear-gradient(to top, #0d0d0d, black); -fx-stroke: #ffffff0e"; //ugly. idk why ->fill and ->strokeColor not working
+        $this->view->add($this->boardRect);
 
         $this->fragment = new UXFragmentPane();
-        $this->fragment->size = $this->mainViewSize;
-        $this->mainView->add($this->fragment);
-
+        $this->fragment->size = $this->viewSize;
+        $this->view->add($this->fragment);
+        
         $form = new contentScene();
-        $form->showInFragment($this->fragment);
+        $this->fragment->applyFragment($form->view);
+        
+        $this->topIndicator();
+    }
+    function topIndicator(){
+        $flowPane = new UXFlowPane();
+        $flowPane->size = [256, 32];
+        $flowPane->alignment = "CENTER";
+        $this->view->add($flowPane);
+        
+        $draggingForm = new DraggingFormBehaviour();
+        $draggingForm->apply($flowPane);        
+
+        $rectIndicator = new UXRectangle();
+        $rectIndicator->size = [64, 4];
+        $rectIndicator->arcWidth = 4;
+        $rectIndicator->arcHeight = 4;
+        $rectIndicator->strokeWidth = 0;
+        $rectIndicator->style = "-fx-fill: #ffffff80"; //another ugly thing
+        $flowPane->add($rectIndicator);
+
     }
 }
