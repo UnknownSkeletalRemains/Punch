feb5dc1b382a34439f89666aacd19b67c31603f8
Kirya <29959059+UnknownSkeletalRemains@users.noreply.github.com>
2023-02-21 18:27:21 +0300

    Add files via upload

--- src/app/core/logger.php
+++ src/app/core/logger.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\core;
 
-use php\{io\Stream, lib\fs, time\Time};
+use php\{io\Stream, lib\fs, lang\System, time\Time};
 
 class logger
 {   
@@ -9,8 +9,13 @@ class logger
     function __construct()
     {
         self::$session = Time::now()->toString('yyyyMMddHHmmss');
+        $osName = System::getProperty('os.name');
+        $osVersion = System::getProperty('os.version');
+        $userName = System::getProperty('user.name');
+        $javaVersion = System::getProperty('java.version');
+        self::log("Operating system: ".$osName." | Version:".$osVersion." | User name: ".$userName." | Java version: ".$javaVersion);
     }
-    public static function log($text = 'NO INFO WAS PUT')
+    public static function log($text = 'Missing argument in ::log()')
     {
         if (fs::exists("./logs")){
             Stream::putContents("logs/".self::$session.".log", Time::now()->toString('yyyy-MM-dd HH:mm:ss').": ".$text."\r\n", "a+");
